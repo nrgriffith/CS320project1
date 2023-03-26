621259ca0134eb3bbe99f40730e17d56c08c9885
nrgri <nrgri@LAPTOP-3O98HK7T.home>
2023-03-25 19:01:54 -0500

    Task.java completed

--- TaskService/src/Task.java
+++ TaskService/src/Task.java
@@ -1,4 +1,64 @@
 
 public class Task {
-
+	private String taskID;
+	private String taskName;
+	private String taskDescription;
+	
+	// Maximum character allowance per the rubric
+	private final int MAX_CHAR_ID = 10;
+	private final int MAX_CHAR_NM = 20;
+	private final int MAX_CHAR_DE = 50;
+	
+	// Iterator to create unique contact IDs
+	private static int idIterator = 0;
+	
+	Task(String id) { // create a task with an id
+		super();
+		if(id != null && id.length() <= MAX_CHAR_ID && !id.isEmpty()) {
+			taskID = id;
+		}
+	}
+	
+	Task() { // default constructor to initialize task
+		taskID = String.valueOf(idIterator);
+		idIterator++;
+		taskName = "name";
+		taskDescription = "description";
+	}
+	
+	// getter methods
+	public String getTaskID() {
+		return taskID;
+	}
+	
+	public String getTaskName() {
+		return taskName;
+	}
+	
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+	
+	// setter methods
+	public Boolean setTaskName(String name) {
+		if(name == null) {
+			return false;
+		}
+		if(!name.isBlank() && name.length() <= MAX_CHAR_NM) {
+			this.taskName = name;
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean setTaskDescription(String description) {
+		if(description == null) {
+			return false;
+		}
+		if(!description.isBlank() && description.length <= MAX_CHAR_DE) {
+			this.taskDescription = description;
+			return true;
+		}
+		return false;
+	}
 }
