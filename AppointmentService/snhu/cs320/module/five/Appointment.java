8b8209ad8e72eed1fbce88867848a1c7655a3c79
nrgri <nrgri@LAPTOP-3O98HK7T.home>
2023-04-02 15:58:04 -0500

    Appointment class finished

--- AppointmentService/snhu/cs320/module/five/Appointment.java
+++ AppointmentService/snhu/cs320/module/five/Appointment.java
@@ -1,5 +1,84 @@
 package snhu.cs320.module.five;
 
-public class Appointment {
+import java.util.Date;
 
+public class Appointment {
+	
+	// required fields
+	private String apptID;
+	private Date apptDate;
+	private String description;
+	
+	// Maximum character allowance per the rubric
+	private final int MAX_CHAR_DE = 50;
+	
+	// Iterator to create unique contact IDs
+	private static int idIterator = 0;
+
+	Appointment(Date date, String descript){
+		apptID = String.valueOf(idIterator);
+		idIterator++;
+		apptDate = date;
+		description = descript;
+	}
+	
+	Appointment(Date date){
+		apptID = String.valueOf(idIterator);
+		idIterator++;
+		apptDate = date;
+		description = "change me";
+	}
+	
+	Appointment(String descript){
+		apptID = String.valueOf(idIterator);
+		idIterator++;
+		apptDate = new Date();
+		description = descript;
+	}
+	
+	Appointment(){
+		apptID = String.valueOf(idIterator);
+		idIterator++;
+		apptDate = new Date();
+		description = "changeme";
+	}
+
+	// Setters methods
+	public Boolean setDescription(String description) {
+		if(description == null) {
+			return false;
+		}
+		else if(!description.isBlank() && description.length() <= MAX_CHAR_DE) {
+			this.description = description;
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean setDate(Date date) {
+		Date curDate = new Date();
+		if(date == null) {
+			return false;
+		}
+		else if(date.before(curDate)) {
+			return false;
+		}
+		else {
+			this.apptDate = date;
+			return true;
+		}
+	}
+	
+	// Getter methods
+	public String getID() {
+		return apptID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getDate() {
+		return apptDate;
+	}
 }
--- AppointmentService/snhu/cs320/module/five/AppointmentTest.java
+++ AppointmentService/snhu/cs320/module/five/AppointmentTest.java
@@ -1,5 +1,46 @@
 package snhu.cs320.module.five;
 
-public class AppointmentTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import java.util.Date;
 
+
+public class AppointmentTest {
+	
+	// yes, this constructor is deprecated but the rubric tells us to use this class
+	// and this is easier than using the Calendar class along with the Date class
+	@SuppressWarnings("deprecation")
+	private Date exDate = new Date(2123-1900, 1-1, 1);
+	@SuppressWarnings("deprecation")
+	private Date pastDate = new Date(2000-1900, 6-1, 6);
+	
+	// some example appointments
+	private Appointment blankAppt = new Appointment();
+	private Appointment dateAppt = new Appointment(exDate);
+	private Appointment fullAppt = new Appointment(exDate, "An appointment");
+	
+	@Test
+	void testApptID() {
+		// cannot be updated
+		Assert.assertThrows(NoSuchFieldException.class, ()->{fullAppt.getClass().getField("apptID");});
+	}
+	
+	@Test
+	void testDescription() {
+		// too long
+		Assert.assertFalse(blankAppt.setDescription("This sentence has over fifty characters and will throw an error when I go to test it."));
+		// empty/null
+		Assert.assertFalse(blankAppt.setDescription(null));
+		Assert.assertFalse(blankAppt.setDescription(""));
+		// just right
+		Assert.assertTrue(blankAppt.setDescription("change oil"));
+	}
+	
+	@Test
+	void testDates() {
+		// in the past
+		Assert.assertFalse(dateAppt.setDate(pastDate));
+		// future date
+		Assert.assertTrue(dateAppt.setDate(exDate));
+	}
 }
