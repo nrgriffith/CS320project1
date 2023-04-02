63d7ba220e66a2fd891b8b0bfdfa884d8d6fcfde
nrgri <nrgri@LAPTOP-3O98HK7T.home>
2023-04-02 18:53:02 -0500

    AppointmentService completed

--- AppointmentService/snhu/cs320/module/five/AppointmentService.java
+++ AppointmentService/snhu/cs320/module/five/AppointmentService.java
@@ -1,5 +1,60 @@
 package snhu.cs320.module.five;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class AppointmentService {
 
+	private ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+	
+	private void addAppt(Appointment appt) { // add appointment to list
+		appointmentList.add(appt);
+	}
+	
+	private Appointment searchAppt(String id) { // search for an appointment ID
+		for (Appointment appt : appointmentList) {
+			if(appt.getID().equals(id)) {
+				return appt;
+			}
+		}
+		return null;
+	}
+	
+	public void createAppt() { // new blank appointment
+		Appointment appt = new Appointment();
+		addAppt(appt);
+	}
+	
+	public void createAppt(Date date) { // new appointment with date
+		Appointment appt = new Appointment(date);
+		addAppt(appt);
+	}
+	
+	public void createAppt(Date date, String description) { // new appointment with date and description
+		Appointment appt = new Appointment(date, description);
+		addAppt(appt);
+	}
+	
+	public void deleteAppt(String id) { // delete appointment from list
+		Appointment appt = searchAppt(id);
+		if(appt != null) {
+			appointmentList.remove(appt);
+		}
+
+	}
+	
+	public ArrayList<Appointment> getAppointments(Date date) { // return appointments on specified date
+		ArrayList<Appointment> returnList = new ArrayList<Appointment>();
+		for(Appointment appointment : appointmentList) {
+			if(appointment.getDate().equals(date)) {
+				returnList.add(appointment);
+			}
+		}
+
+		return returnList;
+	}
+	
+	public int size() {	
+		return appointmentList.size();
+	}
 }
--- AppointmentService/snhu/cs320/module/five/AppointmentServiceTest.java
+++ AppointmentService/snhu/cs320/module/five/AppointmentServiceTest.java
@@ -1,5 +1,54 @@
 package snhu.cs320.module.five;
 
-public class AppointmentServiceTest {
+import java.util.ArrayList;
+import java.util.Date;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class AppointmentServiceTest {
+	
+	AppointmentService appointments = new AppointmentService();
+
+	// yes, this constructor is deprecated but the rubric tells us to use this class
+	// and this is easier than using the Calendar class along with the Date class
+	@SuppressWarnings("deprecation")
+	private Date exDate = new Date(2123-1900, 1-1, 1);
+	@SuppressWarnings("deprecation")
+	private Date exDate2 = new Date(2123-1900, 5-1, 8);
+	
+	public AppointmentServiceTest() { // create some appointments
+		appointments.createAppt(exDate);
+		appointments.createAppt(exDate2, "this is another test");
+	}
+	
+	@Test
+	public void testAdd() { // add an appointment
+		int baseVal = appointments.size();
+		appointments.createAppt();
+		Assert.assertEquals(baseVal+1, appointments.size());
+	}
+	
+	@Test
+	public void testDelete() {
+		// find all appointments with a bad date, should return empty
+		@SuppressWarnings("deprecation")
+		Date pastDate = new Date(2000-1900, 6-1, 6);
+		ArrayList<Appointment> apptOnDate = appointments.getAppointments(pastDate);
+		Assert.assertEquals(0,apptOnDate.size());
+		
+		// find all appointments with date exDate, should return 1
+		apptOnDate = appointments.getAppointments(exDate);
+		Assert.assertEquals(1, apptOnDate.size());
+		
+		// delete all appointments with date exDate
+		for(int i = 0; i < apptOnDate.size(); ++i) {
+			appointments.deleteAppt(apptOnDate.get(i).getID());
+		}
+		
+		// find all appointments with date exDate, should return 0
+		apptOnDate = appointments.getAppointments(exDate);
+		int numAppts = apptOnDate.size();
+		Assert.assertEquals(0, numAppts);
+	}
+	
 }
